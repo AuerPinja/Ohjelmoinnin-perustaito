3a66aa0bd800396d66f9e30cbb9b4c0b177bfff0
Pinja Auer <pinskumelissa@gmail.com>
2023-04-05 14:15:05 +0300

    created arvauspeli

---
+++ Harjoitus1/arvauspeli.py
@@ -0,0 +1,6 @@
+viikonpaiva = input("Anna viikonpäivä: ")
+
+if (viikonpaiva == "Lauantai") or (viikonpaiva == "Sunnuntai") or (viikonpaiva == "lauantai") or (viikonpaiva == "sunnuntai"):
+    print("On viikonloppu!") 
+else:
+    print("On arkipäivä")
