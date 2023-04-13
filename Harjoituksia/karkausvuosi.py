1b9583c8162790142c7263ae34d2dd2c198d0fb5
Pinja Auer <pinskumelissa@gmail.com>
2023-04-13 11:12:04 +0300

    karkausvuosi.py toimii nyt oikein

--- Harjoituksia/karkausvuosi.py
+++ Harjoituksia/karkausvuosi.py
@@ -1,6 +1,6 @@
 vuosi = int(input("Anna vuosi: "))
 
-if (vuosi % 400 == 0) or ((vuosi % 4 == 0) and (vuosi % 100 == 0)):
+if (vuosi % 400 == 0) or ((vuosi % 4 == 0) and (vuosi % 100 != 0)):
     print(f"{vuosi} on karkausvuosi")
 else:
     print(f"{vuosi} ei ole karkausvuosi")
