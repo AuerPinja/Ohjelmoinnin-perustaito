0f374db824def52ea74126b26066b3190ef4a61b
Pinja Auer <pinskumelissa@gmail.com>
2023-05-04 10:32:39 +0300

    added aakkosetjanumerot

---
+++ Harjoituksia/funktioita/AakkosetJaNumerot.py
@@ -0,0 +1,13 @@
+
+def tulostaAakkoset():
+    aakkoset = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "å", "ä", "ö"]
+    for i in aakkoset:
+        print(i)
+
+def tulostaNumerot():
+    numerot = [*range(0, 10)]
+    for i in numerot:
+        print(i)
+
+tulostaAakkoset()
+tulostaNumerot()
